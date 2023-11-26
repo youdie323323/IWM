8cc2f7dc024342ec876e8ced53bbf183da5b6b45
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2023-11-26 13:23:32 +0900

    Update IWM_ObjectsGroup.go

--- IWM_ObjectsGroup.go
+++ IWM_ObjectsGroup.go
@@ -6,14 +6,6 @@ import (
 
 type ObjectsGroup []*Object
 
-// AppendEventToAllObject will append event to all object
-func AppendEventToAllObject[T ObjectsGroup | []*Object](Block T, Events []Event) T {
-	for _, value := range Block {
-		value.Event = append(value.Event, Events...)
-	}
-	return Block
-}
-
 func BuildAllObject[T ObjectsGroup | []*Object](Block T) string {
 	var XML *strings.Builder = new(strings.Builder)
 	for _, obj := range Block {
