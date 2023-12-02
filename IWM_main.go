776419e31e27147897ee46b5b89e1d0859820c71
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2023-12-02 14:47:57 +0900

    Update IWM_main.go

--- IWM_main.go
+++ IWM_main.go
@@ -61,17 +61,4 @@ func (t *Object) BuildXMLObject() string {
 	return string(buf)
 }
 
-func main() {
-	o, n, err := Bright[[]*Object](filepath.Join("assets", "nerd.png"), 810, 624)
-	if err != nil {
-		fmt.Printf("%s", err)
-		return
-	}
-	file, err := os.OpenFile(fmt.Sprintf("numobject=%d.txt", n), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		log.Fatal("Cannot create file", err)
-		return
-	}
-	defer file.Close()
-	file.WriteString(BuildAllObject(o))
-}
+func main() {}
